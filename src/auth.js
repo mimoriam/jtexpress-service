3ffa319f7ca5130a2fa1ecb39f538d39d7a634cc
Mimo <mimoriam420@gmail.com>
2024-06-17 01:58:31 +0500

    Commit #6 - Some more try catch logic

--- src/auth.js
+++ src/auth.js
@@ -2,11 +2,15 @@ import { JWT } from "google-auth-library";
 import credentials from "../durable-micron-426402-a1-017fe82ead1e.json" assert { type: "json" };
 
 const auth = () => {
-  return new JWT({
-    email: credentials.client_email,
-    key: credentials.private_key,
-    scopes: ["https://www.googleapis.com/auth/spreadsheets"],
-  });
+  try {
+    return new JWT({
+      email: credentials.client_email,
+      key: credentials.private_key,
+      scopes: ["https://www.googleapis.com/auth/spreadsheets"],
+    });
+  } catch (err) {
+    console.log("Unable to connect to Google Sheets API. Please check credentials")
+  }
 };
 
 export { auth };

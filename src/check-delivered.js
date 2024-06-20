49e251ab88b01c43844f66707632acbab9e1e303
Mimo <mimoriam420@gmail.com>
2024-06-21 01:36:03 +0500

    Commit #16 - Check for returned status too

--- src/check-delivered.js
+++ src/check-delivered.js
@@ -2,8 +2,8 @@ const checkForDeliveredStatus = async (sheet, currentColumnStr) => {
   const rowStr = currentColumnStr.match(/\d+/g);
   const bStatusCell = sheet.getCellByA1(`B${rowStr}`);
 
-  if (bStatusCell.value === "Delivered") {
-    console.log(`Delivered status at: B${rowStr}`);
+  if (bStatusCell.value === "Delivered" || bStatusCell.value === "Returned") {
+    console.log(`Delivered/Returned status at: B${rowStr}`);
     return "Exists";
   }
   return "Fetching Data... ";
